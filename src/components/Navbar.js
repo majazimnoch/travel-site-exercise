965326e7c538aa299b9f0819a1ff2a6c9044b288
majazimnoch <120421674+majazimnoch@users.noreply.github.com>
2023-09-11 23:00:22 +0200

    made a responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,31 @@
+import { useRef } from 'react';
 import iconBars from '../assets/Hamburger.svg';
 import iconClose from '../assets/Close.svg';
 import styled from 'styled-components'
 
 const Navbar = () => {
+    const navRef = useRef();
+
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+    }
+
     return (
         <header>
             <h3>Scandinavian Adventures</h3>
-            <nav>
+            <nav ref={navRef}>
                 <a href="/experiences">Experiences</a>
                 <a href="/company">Company</a>
                 <a href="/support">Support</a>
-                <StyledButton>
+                <button className="nav-btn nav-close-btn" onClick={showNavbar}>
                     <img src={iconClose} alt="bars burger menu" />
-                </StyledButton>
+                </button>
             </nav>
-            <StyledButton>
+            <button className="nav-btn" onClick={showNavbar}>
                 <img src={iconBars} alt="bars burger menu" />
-            </StyledButton>
+            </button>
         </header>
     );
 };
 
 export default Navbar;
-
-const StyledButton = styled.button`
-background-color: transparent;
-border: none;
-`
